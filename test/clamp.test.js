b6bcf10fc97eb9f9e3ade2798b5fb9a49f69e012
Sampo Järvenpää <sampo.jarvenpaa@tuni.fi>
2025-11-26 12:27:05 +0200

    trying to make pipeline finish even if some tests fail

--- test/clamp.test.js
+++ test/clamp.test.js
@@ -2,35 +2,35 @@ import { expect } from 'chai';
 import clamp from "../src/clamp.js";
 
 describe('clamp', () => {
-    // it('value within range', () => {
-    //     expect(clamp(5, 1, 10)).to.equal(5);
-    // });
+    it('value within range', () => {
+        expect(clamp(5, 1, 10)).to.equal(5);
+    });
 
-    // it("Value within range", () => {
-    //     expect(clamp(5, 1, 10)).to.equal(5);
-    // });
+    it("Value within range", () => {
+        expect(clamp(5, 1, 10)).to.equal(5);
+    });
 
-    // it("Value too large", () => {
-    //     expect(clamp(999, 1, 10)).to.equal(10);
-    // });
+    it("Value too large", () => {
+        expect(clamp(999, 1, 10)).to.equal(10);
+    });
 
-    // it("Negative value", () => {
-    //     expect(clamp(-5, 1, 10)).to.equal(1);
-    // });
+    it("Negative value", () => {
+        expect(clamp(-5, 1, 10)).to.equal(1);
+    });
 
-    // it("Infinity", () => {
-    //     expect(clamp(Infinity, 1, 10)).to.equal(10);
-    // });
+    it("Infinity", () => {
+        expect(clamp(Infinity, 1, 10)).to.equal(10);
+    });
 
-    // it("0 size range", () => {
-    //     expect(clamp(0, 5, 5)).to.equal(5);
-    // });
+    it("0 size range", () => {
+        expect(clamp(0, 5, 5)).to.equal(5);
+    });
 
-    // it("Negative size range", () => {
-    //     expect(clamp(0, 10, 1)).to.equal(null);
-    // });
+    it("Negative size range", () => {
+        expect(clamp(0, 10, 1)).to.equal(null);
+    });
 
-    // it("Negative size range 2", () => {
-    //     expect(clamp(20, 10, 1)).to.equal(null);
-    // });
+    it("Negative size range 2", () => {
+        expect(clamp(20, 10, 1)).to.equal(null);
+    });
 });
