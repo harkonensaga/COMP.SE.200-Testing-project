4af16581a1fae0bdb20e8fbd0a3c2f70488e180e
Olivia Härkönen <olivia.harkonen@tuni.fi>
2025-11-29 15:12:16 +0200

    Rename couple files, add tests for words.js

--- test/toString.test.js
+++ test/toString.test.js
@@ -5,49 +5,48 @@ import toString from "../src/toString.js";
 * This is a test file for toString.js.
 * Tester: Olivia Härkönen
 */
-
 describe("Test toString", () => {
     it("null value", () => {
-        expect(toString(null)).to.equal("");
+        expect(toString(null)).to.eq("");
     });
     
     it("zero", () => {
-        expect(toString(0)).to.equal("0");
+        expect(toString(0)).to.eq("0");
     });
 
     it("minus zero", () => {
-        expect(toString(-0)).to.equal("-0");
+        expect(toString(-0)).to.eq("-0");
     });
 
     it("min value", () => {
-        expect(toString(Number.MIN_VALUE)).to.equal("5e-324");
+        expect(toString(Number.MIN_VALUE)).to.eq("5e-324");
     });
 
     it("max value", () => {
-        expect(toString(Number.MAX_VALUE)).to.equal("1.7976931348623157e+308");
+        expect(toString(Number.MAX_VALUE)).to.eq("1.7976931348623157e+308");
     });
 
     it("infinity", () => {
-        expect(toString(Infinity)).to.equal("Infinity");
+        expect(toString(Infinity)).to.eq("Infinity");
     });
 
     it("negative infinity", () => {
-        expect(toString(-Infinity)).to.equal("-Infinity");
+        expect(toString(-Infinity)).to.eq("-Infinity");
     });
 
     it("array of two", () => {
-        expect(toString([1, 5])).to.equal("1,5");
+        expect(toString([1, 5])).to.eq("1,5");
     });
 
     it("empty array", () => {
-        expect(toString([])).to.equal("");
+        expect(toString([])).to.eq("");
     });
 
     it("string", () => {
-        expect(toString("Seven")).to.equal("Seven");
+        expect(toString("Seven")).to.eq("Seven");
     });
 
     it("symbol", () => {
-        expect(toString(Symbol.iterator)).to.equal("Symbol(Symbol.iterator)");
+        expect(toString(Symbol.iterator)).to.eq("Symbol(Symbol.iterator)");
     });
 });
---
+++ test/words.test.js
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import words from "../src/words.js";
+
+/*
+* This is a test file for words.js.
+* Tester: Olivia Härkönen
+*/
+describe("words", () => {
+    it("splits words without special characters", () => {
+        
+        expect(words("Marley & me")).to.eql(["Marley", "me"]);
+    });
+
+    it("splits words including special characters", () => {
+        expect(words("Marley & me", /[^, ]+/g)).to.eql(["Marley", "&", "me"]);
+    });
+
+    it("ascii basic split", () => {
+        expect(words("test 1 2 3")).to.eql(["test", "1", "2", "3"]);
+    });
+
+    it("string with just special characters", () => {
+        expect(words(" , , & ")).to.eql([]);
+    });
+
+    it("string with no matches for pattern", () => {
+        expect(words("abc", /d+/g)).to.eql([]);
+    });
+});
